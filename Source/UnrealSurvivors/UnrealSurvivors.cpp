04d2c5466fe2e61772e114160787d6c20f7a343d
Mario Jimenez <mjcguada@gmail.com>
2023-01-23 13:21:44 +0100

    Added Unreal Engine project (basic player movement and enemy AI)

---
+++ Source/UnrealSurvivors/UnrealSurvivors.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealSurvivors.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealSurvivors, "UnrealSurvivors" );
